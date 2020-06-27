61f8a918044ca9acad1db878ddfc0d2c8ea3d401
Simon Riley <zhoufengchongyang@outlook.com>
2020-06-28 02:07:35 +0800

    add front pages

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Login></Login>
-    <Tenant></Tenant>
-  </div>
+    <div id="app">
+        <Login></Login>
+        <Tenant></Tenant>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Login from "./components/Login";
-import Tenant from "./components/Tenant";
+    import Login from "./components/Login";
+    import Tenant from "./components/Tenant/Tenant";
+    import Administrator from "./components/Administrator/Administrator";
+    import Manager from "./components/Manager/Manager";
+    import roomAPI from "../api/roomAPI";
 
-export default {
-  name: 'app',
-  components: {
-    Login,
-    Tenant
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            Login,
+            Tenant
+        },
+        data() {
+            let rooms = roomAPI.getRoomList();
+            return {
+                roomList: rooms
+            };
+        },
+        created() {
+            roomAPI.updateRoomList();
+        },
+        beforeDestroy() {
+            roomAPI.releaseAPI();
+        },
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -3,7 +3,7 @@
         <div class="col-lg-6 offset-lg-3">
             <div class="row">
                 <b-input-group>
-                    <b-form-input placeholder="请输入用户名"></b-form-input>
+                    <b-form-input placeholder="请输入用户名" required autofocus></b-form-input>
                     <b-input-group-append>
                         <b-button variant="outline-warning">清除</b-button>
                     </b-input-group-append>
@@ -12,7 +12,7 @@
             <p></p>
             <div class="row">
                 <b-input-group>
-                    <b-form-input type="password" placeholder="请输入密码"></b-form-input>
+                    <b-form-input type="password" placeholder="请输入密码" required></b-form-input>
                     <b-input-group-append>
                         <b-button variant="outline-info">查看</b-button>
                     </b-input-group-append>
@@ -20,7 +20,10 @@
             </div>
             <p></p>
             <div class="row">
-                <b-button variant="outline-primary" pill class="col-lg-2 offset-lg-5">登陆</b-button>
+                <b-checkbox class=" mb-3" value="remember-me">Remember me</b-checkbox>
+            </div>
+            <div class="row">
+                <b-button variant="outline-primary" class="col-lg-2 offset-lg-5">登陆</b-button>
             </div>
         </div>
     </div>
