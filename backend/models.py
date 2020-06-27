8ad61a8641e0a60fb42d55752770b6fd258567e2
KuilongCui <cuikuilong@163.com>
2020-06-27 09:39:09 +0800

    db change to Mysql

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
+
+class Record(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    roomId = models.IntegerField(
+        validators=[
+            MinValueValidator(1)
+        ]
+    )
+    mode = models.IntegerField(
+        default=0,
+        validators=[
+            MaxValueValidator(128),
+            MinValueValidator(1)
+        ]
+    )
+    startTime = models.DateTimeField()
+    endTime = models.DateField()
+    fanSpeed = models.IntegerField(
+        default=2,
+        validators=[
+            MaxValueValidator(3),
+            MinValueValidator(1)
+        ]
+    )
+    fee = models.FloatField(
+        validators=[
+            MinValueValidator(0.0)
+        ]
+    )
 # Create your models here.
--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from backend import models
+
 
 # Create your views here.
+
+def fun(request):
+    t = models.Record.objects.all()
+    print(t)
+    return HttpResponse("ok")
